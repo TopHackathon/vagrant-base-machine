917c8842d94e836ed942c7babdca6811341807b3
Jesper Wermuth <wermuth@lundogbendsen.dk>
2015-06-16 15:59:30 +0200

    Can start machine with IP

--- docker.sh
+++ docker.sh
@@ -5,4 +5,5 @@ echo "installing docker"
 sudo apt-get install curl -y
 sudo curl -sSL https://get.docker.com/ubuntu/ | sudo sh
 echo 'DOCKER_OPTS="-H=unix:///var/run/docker.sock -H=0.0.0.0:4243"' >> /etc/default/docker
-
+sudo restart docker
+# sudo service docker reload !!!
--- tie.sh
+++ tie.sh
@@ -2,6 +2,7 @@
 
 MACHINE=$1 vagrant destroy --force
 MACHINE=$1 vagrant up
-IP=$(vagrant ssh $1 -c 'ifconfig eth1 | grep "inet "' | grep "inet " | cut -f 2 -d ":" | cut -d " " -f 1)
+IP=$(MACHINE=$1 vagrant ssh -c 'ifconfig eth1 | grep "inet "' | grep "inet " | cut -f 2 -d ":" | cut -d " " -f 1)
 docker -H $IP:4243 $2
+echo Congratulation, you can tie your tie on $IP
 
