3db96f9c76dab2e9334515ec01ceafde40f90584
Jesper Wermuth <wermuth@lundogbendsen.dk>
2015-06-16 15:43:14 +0200

    merged

--- tie.sh
+++ tie.sh
@@ -2,7 +2,6 @@
 
 MACHINE=$1 vagrant destroy --force
 MACHINE=$1 vagrant up
-# IP = get IP
 IP=$(vagrant ssh $1 -c 'ifconfig eth1 | grep "inet "' | grep "inet " | cut -f 2 -d ":" | cut -d " " -f 1)
 docker -H $IP:4243 $2
 
