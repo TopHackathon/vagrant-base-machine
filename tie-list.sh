7b71e64fe0d5da23a19d97c58b42bbbf380fa11a
Jesper Wermuth <wermuth@lundogbendsen.dk>
2015-06-17 11:58:19 +0200

    Better usage message

--- tie-list.sh
+++ tie-list.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
+if [ $# -eq 0 ]; then
+echo "Usage: $0 -> list of running images"
+echo ""
+echo $0 "creates a docker host and gives you back the IP of the host."
+echo ""
+echo "Example" $0 "my-machine run -e GIT.URL==https://github.com/TopHackathon/example-app.git -p 8080:8080 tophackathon/ci-java8:1.0"
+echo ""
+echo "You can input any docker command you want. It will be executed on the newly created docker host."
+exit 1
+fi
+
 vagrant global-status
--- tie-up.sh
+++ tie-up.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 if [ $# -eq 0 ]; then
-echo "Usage: $0 machine-name docker-command -> IP"
+echo "Usage: $0 machine-name \"docker-parameters-and-options\" -> IP"
 echo ""
 echo $0 "creates a docker host and gives you back the IP of the host."
 echo ""
-echo "Example" $0 "my-machine run -e GIT.URL==https://github.com/TopHackathon/example-app.git -p 8080:8080 tophackathon/ci-java8:1.0"
+echo "Example" $0 "my-machine \"run -e GIT.URL==https://github.com/TopHackathon/example-app.git -p 8080:8080 tophackathon/ci-java8:1.0\""
 echo ""
 echo "You can input any docker command you want. It will be executed on the newly created docker host."
 exit 1
