37393cb8c6d1ee1e30fb68bf48fabfa039b14c40
Jesper Wermuth <wermuth@lundogbendsen.dk>
2015-07-09 14:39:13 +0200

    Can access local registry at 192.168.9.5:5000

--- docker-configure.sh
+++ docker-configure.sh
@@ -2,4 +2,4 @@
 
 echo "Configuring  docker"
 
-echo 'DOCKER_OPTS="-H=unix:///var/run/docker.sock -H=0.0.0.0:4243  --insecure-registry 192.168.1.24:5000"' >> /etc/default/docker
+echo 'DOCKER_OPTS="-H=unix:///var/run/docker.sock -H=0.0.0.0:4243  --insecure-registry 192.168.9.5:5000"' >> /etc/default/docker
